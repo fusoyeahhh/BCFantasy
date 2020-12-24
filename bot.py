02a256ec657aa2fff50cce0e75dfdf37a2e73bf4
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2020-12-23 19:10:12 -0800

    bot.py: restore info log level

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import numpy
 import pandas
 import json
 import logging
-logging.basicConfig(stream=sys.stdout, level=logging.WARN)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 from twitchio.ext import commands
 from twitchio.dataclasses import User
 import glob
