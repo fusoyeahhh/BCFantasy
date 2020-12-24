b53cc5babad4f6a492d629c62dc566d4d0010353
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2020-12-23 17:41:12 -0800

    bot.py: reduce logging to warn and more severe

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import numpy
 import pandas
 import json
 import logging
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.WARN)
 from twitchio.ext import commands
 from twitchio.dataclasses import User
 import glob
