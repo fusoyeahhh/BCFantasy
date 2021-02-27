8be14e238686c6d7d6251be85e275c8a85155702
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-02-27 12:24:48 -0800

    bot.py: move chunk_string to utils

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ import glob
 
 from bcf import _ACTOR_MAP
 from bcf import read
+from bcf.utils import _chunk_string
 from bcf.utils import export_to_gsheet
 
 #
@@ -455,42 +456,6 @@ def _set_context(content):
 
     # Indicate success
     return True
-
-def _chunk_string(inlist, joiner=", "):
-    """
-    Given a list of strings to (ultimately) concatenate and send to twitch chat.
-
-    The list of strings is concatenated (via 'joiner') into strings of lengths that are allowed by twitch chat.
-
-    :param inlist: (list) list of strings to emit
-    :param joiner: (str) character to use a joiner
-    :return: None
-    """
-
-    # Nothing to do here
-    if len(inlist) == 0:
-        return
-
-    # There's a string in the list which is larger than the allowed count
-    # FIXME: we can break up this string too, if needed, possibly by calling this recursively
-    assert max([*map(len, inlist)]) < 500, \
-                                "Can't fit all messages to buffer length"
-
-    outstr = str(inlist.pop(0))
-    # While we have strings to send
-    while len(inlist) >= 0:
-        # We've reached the end of the input list, drain the remaining buffer and end
-        if len(inlist) == 0:
-            yield outstr
-            return
-        # If the next concat would put us over the limit, then we emit, reset, and continue
-        elif len(outstr) + len(joiner) + len(inlist[0]) >= 500:
-            yield outstr
-            outstr = inlist.pop(0)
-            continue
-
-        # continue concatenating
-        outstr += joiner + str(inlist.pop(0))
 
 def _check_term(term, lookup, info, space_suppress=True, full=False, allow_multiple=False):
     """
