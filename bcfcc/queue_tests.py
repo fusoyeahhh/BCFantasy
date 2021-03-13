738469765e0fc1f238a1912926ccf522a74c90a6
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-03-13 09:56:00 -0800

    queue_tests.py: test suite for command queue

---
+++ bcfcc/queue_tests.py
@@ -0,0 +1,72 @@
+import time
+from bcfcc import activate_golem, add_gp
+from bcfcc.cmdimpl import AddGP, MoogleCharm, Remedy, RandomRelicEffect, MirrorButtons
+from bcfcc.queue import CCQueue
+
+if __name__ == '__main__':
+    ccq = CCQueue(memfile="memfile")
+
+    # With actual CC commands
+    # This one is convenient because it has no arguments to preserve
+    ccq.make_task(activate_golem, name='activate_golem', user="test", state="battle")
+    print("[activate_golem] Checking state logic")
+    ccq.check(None, ignore_completion=True)
+    ccq.check({"in_battle": True}, ignore_completion=True)
+
+    ccq.make_task(add_gp, name="add_gp", user="test")
+    print("[add_gp] Checking queue execution logic")
+    ccq.check(None, ignore_completion=True)
+
+    ccq.reset()
+
+    # Generate game context
+    gctx = ccq.construct_game_context()
+    game_state = {"in_battle": True}
+
+    # Generate a new instance of CC command
+    print("[AddGP] Checking queue execution logic")
+    req = AddGP("test")
+    req._add_to_queue(ccq)
+    print(ccq.write())
+    ccq.check(game_state, ignore_completion=True)
+    print(ccq.write())
+    ccq.reset()
+
+    print("[Remedy] Checking in_battle logic")
+    req = Remedy("test")
+    req._add_to_queue(ccq, 1)
+    print(ccq.write())
+    ccq.check(game_state, ignore_completion=True)
+    print(ccq.write())
+    ccq.reset()
+
+    print("[RandomRelicEffect] Checking random relic effects")
+    req = RandomRelicEffect("test")
+    req._add_to_queue(ccq)
+    print(ccq.write())
+    ccq.check(game_state, ignore_completion=True)
+    print(ccq.write())
+    ccq.reset()
+
+    print("[MoogleCharm] Checking queue delay and effect duration extension logic")
+    req = MoogleCharm("test", duration=10)
+    req._add_to_queue(ccq)
+    ccq.check(game_state, ignore_completion=True)
+    print(ccq.write())
+    print("Sleeping for 2 seconds to check duration logic")
+    time.sleep(2)
+    ccq.check(game_state, ignore_completion=True)
+    print(ccq.write())
+    print("Increasing duration by 10 seconds.")
+    req._add_to_queue(ccq)
+    print(ccq.write())
+
+    print("[MirrorButtons] Checking button trans / inverse trans")
+    req = MirrorButtons("test", duration=1)
+    req._add_to_queue(ccq)
+    ccq.check(game_state, ignore_completion=True)
+    print(ccq.write())
+    print("Sleeping for 2 seconds to check duration logic")
+    time.sleep(2)
+    ccq.check(game_state, ignore_completion=True)
+    print(ccq.write())
