3b52c2dada9b2eedbbf70d8b7b375c38a4b3927b
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-03-22 19:26:49 -0700

    bot.py: enforce preconditions

--- bot.py
+++ bot.py
@@ -1589,8 +1589,11 @@ if _ENABLE_CC:
         try:
             if not task.precondition(*args):
                 raise ValueError("Precondition failed")
-        except:
-            logging.warning(f"cc | Precondition for task {task.label} not met, in the future this will be an error.")
+        except Exception as e:
+            logging.warning(f"cc | Precondition for task {task.label} not met.")
+            logging.warning(str(e))
+            await ctx.send(f"@{user}: check your command, you may need additional arguments.")
+            return
 
         logging.info(f"cc | Deducting cost for command {cmd} ({task.cost}) from {user}")
         if user not in _USERS:
