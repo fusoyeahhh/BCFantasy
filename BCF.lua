8f23ca36eded59162149194d8a58f2e29c7ba233
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-05-23 14:24:31 -0700

    BCF.lua: debug --- determine reason for enemy death

--- BCF.lua
+++ BCF.lua
@@ -265,6 +265,7 @@ while true do
 				and (curr_hp == 0 or is_wounded or is_zombied or is_petrified)
 				and nenem_alive < enemies_alive then
 			status = status .. c_last_targetted
+			print((curr_hp == 0) .. " " .. is_wounded .. " " .. is_zombied .. " " .. is_petrified)
 
 			-- Attribute kill to the last character that targetted this
 			if c_last_targetted ~= nil then
